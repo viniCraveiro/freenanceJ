78472d21461443d36ec01adbe4892bc5e931b757
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2023-05-30 22:39:01 -0300

    Projeto inicial

---
+++ src/test/java/com/craveiro/freenanceJ/FreenanceJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craveiro.freenanceJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreenanceJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
